094aaae59e0b670b8474f6cce2685f7f8b065087
doongjohn <doongjohn@gmail.com>
2025-07-27 07:52:24 +0900

    update build.zig

--- build.zig
+++ build.zig
@@ -10,73 +10,24 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
         .link_libc = true,
     });
-
     const exe = b.addExecutable(.{
         .name = "learn-zig-sdl3",
         .root_module = exe_mod,
     });
     exe.subsystem = .Console;
+    b.installArtifact(exe);
 
-    // create vendor dir
+    // Create vendor dir
     std.fs.cwd().makeDir("vendor") catch |err| switch (err) {
         error.PathAlreadyExists => {},
         else => return err,
     };
 
-    // SDL3 paths
-    const sdl3_src_dir = "vendor/SDL/";
-    const sdl3_include_dir = sdl3_src_dir ++ "include";
-    const sdl3_build_dir = switch (optimize) {
-        .Debug => sdl3_src_dir ++ "build/clang_debug/",
-        else => sdl3_src_dir ++ "build/clang_release/",
-    };
-
-    // SDL3 git clone
-    const sdl3_git_clone = b.addSystemCommand(&.{ "git", "clone", "-b", "release-3.2.18", "--depth=1", "https://github.com/libsdl-org/SDL.git" });
-    sdl3_git_clone.cwd = b.path("vendor/");
-
-    // SDL3 cmake configure
-    const sdl3_cmake_conf = b.addSystemCommand(&.{ "cmake", "-S", sdl3_src_dir, "-B", sdl3_build_dir, "-G", "Ninja" });
-    sdl3_cmake_conf.setEnvironmentVariable("CC", "clang");
-    sdl3_cmake_conf.setEnvironmentVariable("CXX", "clang++");
-    switch (optimize) {
-        .Debug => sdl3_cmake_conf.addArg("-DCMAKE_BUILD_TYPE=Debug"),
-        else => sdl3_cmake_conf.addArg("-DCMAKE_BUILD_TYPE=Release"),
-    }
-    _ = std.fs.cwd().access(sdl3_src_dir, .{}) catch |err| switch (err) {
-        error.FileNotFound => sdl3_cmake_conf.step.dependOn(&sdl3_git_clone.step),
-        else => return err,
-    };
-
-    // SDL3 cmake build
-    const sdl3_cmake_build = b.addSystemCommand(&.{ "cmake", "--build", sdl3_build_dir });
-    _ = std.fs.cwd().access(sdl3_build_dir, .{}) catch |err| switch (err) {
-        error.FileNotFound => sdl3_cmake_build.step.dependOn(&sdl3_cmake_conf.step),
-        else => return err,
-    };
-
-    // SDL3 install dll
-    switch (target.result.os.tag) {
-        .windows => {
-            const sdl3_dll_path = b.pathJoin(&.{ sdl3_build_dir, "SDL3.dll" });
-            const sdl3_install_dll = b.addInstallBinFile(b.path(sdl3_dll_path), "SDL3.dll");
-            sdl3_install_dll.step.dependOn(&sdl3_cmake_build.step);
-            b.getInstallStep().dependOn(&sdl3_install_dll.step);
-        },
-        else => {
-            @panic("TODO");
-        },
-    }
-
-    // SDL3 link
-    {
-        exe.step.dependOn(&sdl3_cmake_build.step);
-        exe.addIncludePath(b.path(sdl3_include_dir));
-        exe.addLibraryPath(b.path(sdl3_build_dir));
-        exe.linkSystemLibrary("SDL3");
-    }
-
-    b.installArtifact(exe);
+    // SDL3
+    var lib_sdl3 = LibSdl3.init(b, target, optimize);
+    try lib_sdl3.build("release-3.2.18");
+    lib_sdl3.link(exe);
+    lib_sdl3.install();
 
     // Run exe
     const run_step = b.step("run", "Run the app");
@@ -97,3 +48,91 @@ pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
+
+const LibSdl3 = struct {
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+
+    source_dir: []const u8,
+    include_dir: []const u8,
+    build_dir: []const u8,
+
+    cmake_build: ?*std.Build.Step.Run = null,
+
+    pub fn init(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) @This() {
+        const source_dir = "vendor/SDL/";
+        const include_dir = source_dir ++ "include/";
+        const build_dir = switch (optimize) {
+            .Debug => source_dir ++ "build/clang_debug/",
+            else => source_dir ++ "build/clang_release/",
+        };
+
+        return .{
+            .b = b,
+            .target = target,
+            .optimize = optimize,
+            .source_dir = source_dir,
+            .include_dir = include_dir,
+            .build_dir = build_dir,
+        };
+    }
+
+    pub fn build(this: *@This(), git_tag: []const u8) !void {
+        const b = this.b;
+
+        // git clone
+        const git_url = "https://github.com/libsdl-org/SDL.git";
+        const git_clone = b.addSystemCommand(&.{ "git", "clone", "--depth=1", "-b", git_tag, git_url });
+        git_clone.cwd = b.path("vendor/");
+
+        // cmake configure
+        const cmake_conf = b.addSystemCommand(&.{ "cmake", "-S", this.source_dir, "-B", this.build_dir, "-G", "Ninja" });
+        cmake_conf.setEnvironmentVariable("CC", "clang");
+        cmake_conf.setEnvironmentVariable("CXX", "clang++");
+        switch (this.optimize) {
+            .Debug => cmake_conf.addArg("-DCMAKE_BUILD_TYPE=Debug"),
+            else => cmake_conf.addArg("-DCMAKE_BUILD_TYPE=Release"),
+        }
+        _ = std.fs.cwd().access(this.source_dir, .{}) catch |err| switch (err) {
+            error.FileNotFound => cmake_conf.step.dependOn(&git_clone.step),
+            else => return err,
+        };
+
+        // cmake build
+        this.cmake_build = b.addSystemCommand(&.{ "cmake", "--build", this.build_dir });
+        _ = std.fs.cwd().access(this.build_dir, .{}) catch |err| switch (err) {
+            error.FileNotFound => this.cmake_build.?.step.dependOn(&cmake_conf.step),
+            else => return err,
+        };
+    }
+
+    pub fn link(this: *@This(), exe: *std.Build.Step.Compile) void {
+        std.debug.assert(this.cmake_build != null);
+
+        const b = this.b;
+
+        exe.step.dependOn(&this.cmake_build.?.step);
+        exe.addIncludePath(b.path(this.include_dir));
+        exe.addLibraryPath(b.path(this.build_dir));
+        exe.linkSystemLibrary("SDL3");
+    }
+
+    pub fn install(this: *@This()) void {
+        std.debug.assert(this.cmake_build != null);
+
+        const b = this.b;
+
+        switch (this.target.result.os.tag) {
+            .windows => {
+                const dll_path = b.pathJoin(&.{ this.build_dir, "SDL3.dll" });
+                const dll_install = b.addInstallBinFile(b.path(dll_path), "SDL3.dll");
+                dll_install.step.dependOn(&this.cmake_build.?.step);
+                b.getInstallStep().dependOn(&dll_install.step);
+            },
+            else => {
+                @panic("TODO");
+            },
+        }
+    }
+};

409273725fb1259a89ff14512e8b76fa536d8714
doongjohn <doongjohn@gmail.com>
2026-02-23 19:28:20 +0900

    update build.zig

--- build.zig
+++ build.zig
@@ -8,21 +8,20 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
     const exe = b.addExecutable(.{
         .name = "learn-zig-sdl3",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
     });
     exe.subsystem = .Console;
     b.installArtifact(exe);
 
     // Create vendor dir
-    std.Io.Dir.cwd().createDir(io, "vendor", .default_dir) catch |err| switch (err) {
+    std.Io.Dir.cwd().createDirPath(io, "vendor") catch |err| switch (err) {
         error.PathAlreadyExists => {},
         else => return err,
     };
@@ -44,7 +43,7 @@ pub fn build(b: *std.Build) !void {
 
     // Tests
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_module = exe.root_module,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
